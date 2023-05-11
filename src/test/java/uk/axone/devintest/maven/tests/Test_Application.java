02172923357fc2070d795c003180a05062044b4c
shashikirana <shashikiranadeepak@gmail.com>
2023-05-11 11:33:26 +0100

    Jira- 1234 - First commit for new project using Jenkins

---
+++ src/test/java/uk/axone/devintest/maven/tests/Test_Application.java
@@ -0,0 +1,30 @@
+package uk.axone.devintest.maven.tests;
+
+import org.junit.Test;
+public class Test_Application {
+
+    @Test
+
+
+    public void test1(){
+        System.out.println("running test 1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("running test 2");
+
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("running test 3");
+    }
+
+    @Test
+    public void test4(){
+        System.out.println("running test 4");
+    }
+
+
+}
